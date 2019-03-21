06e4ad6a8120f08c2aeb08ed631ad67c27674eab
Aneisha Mallikaratchy <46450865+Aneisha-m@users.noreply.github.com>
2019-03-21 15:44:28 +0000

    initial commit

---
+++ BbcLoginSteps.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using NUnit.Framework;
+
+namespace Eng24SeleniumSpecflow
+{
+    [Binding]
+    public class BbcLoginSteps
+    {
+        private IWebDriver _driver;
+        private BbcLoginPage _bbcLoginPage;
+
+        [Given(@"I am on the login page")]
+        public void GivenIAmOnTheLoginPage()
+        {
+            _driver = new ChromeDriver();
+            _driver.Manage().Window.Maximize();
+            _bbcLoginPage = BbcLoginPage.NavigateTo(_driver);
+            //_driver.Navigate().GoToUrl("http://bbc.co.uk");
+            //IWebElement loginButton = _driver.FindElement(By.Id("idcta-link"));
+            //loginButton.Click();
+        }
+        
+        [Given(@"I entered a valid (.*)")]
+        public void GivenIEnteredAValidUsername(string username)
+        {
+            _bbcLoginPage.Username = "user@username.com";
+        }
+        
+        [Given(@"I have entered a invalid (.*)")]
+        public void GivenIHaveEnteredAInvalidPassword(string password)
+        {
+            _bbcLoginPage.Password = "12345678";
+        }
+        
+        [When(@"I press login")]
+        public void WhenIPressLogin()
+        {
+            _bbcLoginPage.ClickSignin();
+        }
+        
+        [Then(@"I shlould see the appropriate (.*)")]
+        public void ThenIShlouldSeeTheAppropriateError(string error)
+        {
+            Assert.AreEqual("Sorry, that password isn't valid. Please include a letter.", _bbcLoginPage.ErrorText);
+        }
+
+        [AfterScenario]
+        public void DisposeWebDriver()
+        {
+            _driver.Dispose();
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Eng24SeleniumSpecflow")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("Eng24SeleniumSpecflow")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7efbb644-c9b7-4d67-9555-cb520010526f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
